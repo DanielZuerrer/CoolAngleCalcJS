693d0184031f578e2c0018718856bed10a8b6a53
Daniel Zuerrer <daniel.zuerrer@gmail.com>
2016-11-24 16:27:00 +0100

    added second image functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 var imageLoader = document.getElementById('filePhoto');
 imageLoader.addEventListener('change', handleImage, false);
+var secondImageLoader = document.getElementById('secondPhoto');
+secondImageLoader.addEventListener('change', handleSecondImage, false);
+
+var firstImageSource, secondImageSource;
 
 function handleImage(e) {
     var reader = new FileReader();
     reader.onload = function (event) {
         $('#uploader').remove();
         $('#filePhoto').remove();
-        $('#structure').attr('src', event.target.result);
+        firstImageSource = event.target.result
+        $('#structure').attr('src', firstImageSource);
         $('#mainContainer').height($('#structure').height())
-        $('#structureContainer').width($('#structure').width())
+        $('.structureContainer').width($('#structure').width())
         $('.hidden').toggleClass("hidden")
 
+    };
+    reader.readAsDataURL(e.target.files[0]);
+
+}
+function handleSecondImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        $('#secondUploader').remove();
+        $('#secondPhoto').remove();
+        secondImageSource = event.target.result
+        $('#structure').attr('src', secondImageSource);
+        $('.hidden-switch').toggleClass('hidden-switch')
     };
     reader.readAsDataURL(e.target.files[0]);
 
@@ -22,12 +39,19 @@ dropbox.addEventListener("dragenter", dragenter, false);
 dropbox.addEventListener("dragover", dragover, false);
 dropbox.addEventListener("drop", drop, false);
 
+var secondDropbox = document.getElementById("secondUploader");
+secondDropbox.addEventListener("dragenter", dragenter, false);
+secondDropbox.addEventListener("dragover", dragover, false);
+secondDropbox.addEventListener("drop", drop2, false);
+
 function dragenter(e) {
+    console.log('dragenter')
     e.stopPropagation();
     e.preventDefault();
 }
 
 function dragover(e) {
+    console.log('dragover')
     e.stopPropagation();
     e.preventDefault();
 }
@@ -41,6 +65,16 @@ function drop(e) {
 
     //this code line fires your 'handleImage' function (imageLoader change event)
     imageLoader.files = files;
+}
+function drop2(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    //you can check e's properties console.log(e);
+    var dt = e.dataTransfer;
+    var files = dt.files;
+
+    //this code line fires your 'handleImage' function (imageLoader change event)
+    secondImageLoader.files = files;
 }
 
 var degreesCanvas;
@@ -65,8 +99,8 @@ var redTimesX,
     redTimesY;
 
 function addRedLineListeners() {
-    $('#schabloneContainer').draggable()
-    $('#schablone').resizable({
+    $('.schabloneContainer').draggable()
+    $('.schablone').resizable({
         containment: "#mainContainer",
         handles: "e, s, n, w",
         resize: function (event, ui) {
@@ -86,7 +120,7 @@ function addRedLineListeners() {
         }
     })
 
-    $('#rotateContainer').propeller({
+    $('.rotateContainer').propeller({
         onRotate: function () {
             redTriangle.baseAngle = this.angle;
             redLine.angle = redTriangle.angle + redTriangle.baseAngle;
@@ -101,9 +135,9 @@ var greenTimesX,
     greenTimesY;
 
 function addGreenLineListeners() {
-    $('#lineContainer').draggable()
+    $('.lineContainer').draggable()
 
-    $('#lineRotateContainer').propeller({
+    $('.lineRotateContainer').propeller({
         onRotate: function () {
             greenLine.angle = this.angle;
             updateCanvas();
@@ -112,11 +146,11 @@ function addGreenLineListeners() {
 }
 
 function fixRedPosition() {
-    $('#schabloneContainer').draggable("destroy");
-    $('#schablone').resizable("destroy");
+    $('.schabloneContainer').draggable("destroy");
+    $('.schablone').resizable("destroy");
 }
 function fixGreenPosition() {
-    $('#lineContainer').draggable("destroy");
+    $('.lineContainer').draggable("destroy");
 }
 
 function canvasInit() {
@@ -155,14 +189,22 @@ function updateCanvas() {
 function flipSchablone() {
     flipped = !flipped;
     if (flipped) {
-        $('#schablone').attr('src', 'img/schabloneR.png');
+        $('.schablone').attr('src', 'img/schabloneR.png');
         redTriangle.angle = -redTriangle.angle;
     } else {
-        $('#schablone').attr('src', 'img/schabloneL.png');
+        $('.schablone').attr('src', 'img/schabloneL.png');
         redTriangle.angle = -redTriangle.angle;
     }
     redLine.angle = redTriangle.angle + redTriangle.baseAngle;
     updateCanvas();
+}
+
+function switchImages() {
+    if ($('#structure').attr('src') == firstImageSource) {
+        $('#structure').attr('src',secondImageSource)
+    } else {
+        $('#structure').attr('src',firstImageSource)
+    }
 }
 
 function round(value, decimals) {
